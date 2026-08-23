cc80d3dd4c44674e47c4a480719f6f925c115c66
IngiRG <igasadal@gmail.com>
2026-08-23 14:30:56 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  // Relative paths make the generated site work at
-  // https://USERNAME.github.io/REPOSITORY-NAME/
-  // as well as at a custom domain or local preview.
-  base: "./",
-});
