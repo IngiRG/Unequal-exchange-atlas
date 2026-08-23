ab2b16063577cf4e69074208c9cd595f33064e0b
IngiRG <igasadal@gmail.com>
2026-08-23 14:15:14 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Relative paths make the generated site work at
+  // https://USERNAME.github.io/REPOSITORY-NAME/
+  // as well as at a custom domain or local preview.
+  base: "./",
+});
